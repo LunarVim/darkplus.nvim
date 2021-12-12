d07db6558ee7358a28195d3100db605e14212cb4
ChristianChiarulli <chris.machine@pm.me>
2021-12-12 11:36:49 -0500

    inactive fg

--- lua/lualine/themes/darkplus.lua
+++ lua/lualine/themes/darkplus.lua
@@ -29,8 +29,8 @@ return {
   command = {a = {fg = colors.fg, bg = colors.bg, gui = 'bold'}},
   replace = {a = {fg = colors.fg, bg = colors.bg, gui = 'bold'}},
   inactive = {
-    a = {fg = colors.gray1, bg = colors.bg, gui = 'bold'},
-    b = {fg = colors.gray1, bg = colors.bg},
-    c = {fg = colors.gray1, bg = colors.bg}
+    a = {fg = colors.fg, bg = colors.bg},
+    b = {fg = colors.fg, bg = colors.bg},
+    c = {fg = colors.fg, bg = colors.bg}
   }
 }
