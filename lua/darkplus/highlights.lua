5163901c86e008fb819607f9c5391bda2490eb05
Oleg Lelenkov <olelenkov@gmail.com>
2022-05-21 23:01:19 +0300

    Fix FloatBorder highlight

--- lua/darkplus/highlights.lua
+++ lua/darkplus/highlights.lua
@@ -17,7 +17,7 @@ local highlights = {
 		Folded = {fg = C.accent, bg = C.alt_bg, },
 		FoldColumn = {fg = C.accent, bg = C.alt_bg, },
 		LineNr = {fg = C.gray, },
-		FloatBoder = {fg = C.gray, bg = C.alt_bg, },
+		FloatBorder = {fg = C.gray, bg = C.alt_bg, },
 		Whitespace = {fg = C.bg, },
 		VertSplit = {fg = C.gray, bg = C.bg, },
 		CursorLine = {bg = C.dark, },
