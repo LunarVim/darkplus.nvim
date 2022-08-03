699594055893c506da12838e9f2f5ecd84a471b4
ChristianChiarulli <chris.machine@pm.me>
2022-08-03 01:17:47 -0400

    update colorscheme to use colorgen-nvim

--- colors/darkplus.vim
+++ colors/darkplus.vim
@@ -1,27 +1,4 @@
-" Author: Christian Chiarulli <chrisatmachine@gmail.com>
-
 lua << EOF
-package.loaded['darkplus'] = nil
-package.loaded['darkplus.highlights'] = nil
-package.loaded['darkplus.Treesitter'] = nil
-package.loaded['darkplus.markdown'] = nil
-package.loaded['darkplus.Whichkey'] = nil
-package.loaded['darkplus.Git'] = nil
-package.loaded['darkplus.LSP'] = nil
-package.loaded['darkplus.Quickscope'] = nil
-package.loaded['darkplus.Telescope'] = nil
-package.loaded['darkplus.NvimTree'] = nil
-package.loaded['darkplus.Lir'] = nil
-package.loaded['darkplus.Buffer'] = nil
-package.loaded['darkplus.StatusLine'] = nil
-package.loaded['darkplus.IndentBlankline'] = nil
-package.loaded['darkplus.Dashboard'] = nil
-package.loaded['darkplus.DiffView'] = nil
-package.loaded['darkplus.Bookmarks'] = nil
-package.loaded['darkplus.Bqf'] = nil
-package.loaded['darkplus.Cmp'] = nil
-package.loaded['darkplus.SymbolOutline'] = nil
-package.loaded['darkplus.Misc'] = nil
-
-require("darkplus")
+local darkplus = require("darkplus")
+darkplus.setup({})
 EOF
--- lua/darkplus/init.lua
+++ lua/darkplus/init.lua
@@ -1,44 +1,18 @@
-vim.api.nvim_command("hi clear")
-if vim.fn.exists("syntax_on") then
-    vim.api.nvim_command("syntax reset")
+local M = {}
+local theme = require('darkplus.theme')
+
+M.setup = function()
+  vim.cmd('hi clear')
+
+  vim.o.background = 'dark'
+  if vim.fn.exists('syntax_on') then
+    vim.cmd('syntax reset')
+  end
+
+  vim.o.termguicolors = true
+  vim.g.colors_name = 'darkplus'
+
+  theme.set_highlights()
 end
-vim.o.background = "dark"
-vim.o.termguicolors = true
-vim.g.colors_name = "darkplus"
 
-local util = require("darkplus.util")
-Config = require("darkplus.config")
-C = require("darkplus.palette")
-local highlights = require("darkplus.highlights")
-local Treesitter = require("darkplus.Treesitter")
-local markdown = require("darkplus.markdown")
-local Whichkey = require("darkplus.Whichkey")
-local Git = require("darkplus.Git")
-local LSP = require("darkplus.LSP")
-local Quickscope = require("darkplus.Quickscope")
-local Telescope = require("darkplus.Telescope")
-local NvimTree = require("darkplus.NvimTree")
-local Lir = require("darkplus.Lir")
-local Buffer = require("darkplus.Buffer")
-local StatusLine = require("darkplus.StatusLine")
-local IndentBlankline = require("darkplus.IndentBlankline")
-local Dashboard = require("darkplus.Dashboard")
-local DiffView = require("darkplus.DiffView")
-local Bookmarks = require("darkplus.Bookmarks")
-local Bqf = require("darkplus.Bqf")
-local Cmp = require("darkplus.Cmp")
-local Gps = require("darkplus.Gps")
-local Packer = require("darkplus.Packer")
-local SymbolOutline = require("darkplus.SymbolOutline")
-local Notify = require("darkplus.Notify")
-local Misc = require("darkplus.Misc")
-local Crates = require("darkplus.Crates")
-
-
-local skeletons = {
-    highlights, Treesitter, markdown, Whichkey, Git, LSP, Quickscope, Telescope, NvimTree, Lir, Buffer, StatusLine, IndentBlankline, Dashboard, DiffView, Bookmarks, Bqf, Cmp, Gps, Packer, SymbolOutline, Notify, Misc, Crates
-}
-
-for _, skeleton in ipairs(skeletons) do
-    util.initialise(skeleton)
-end
+return M
